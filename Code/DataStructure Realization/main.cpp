0d54a1c567f2363620013fd2c7b3c5ec66013abf
ZYBO-o <15298392615@163.com>
2021-01-16 10:40:20 +0800

    2021/1/16 first

--- Code/DataStructure Realization/main.cpp
+++ Code/DataStructure Realization/main.cpp
@@ -16,16 +16,25 @@ public:
     }
 };
 
+double divide(double a, double b)
+{
+    //因为浮点数的表述是不精确的，所以最好不要通过等号的方式进行比较
+    const double delta = 0.000000000000001;
+    double ret = 0;
+    if (!((-delta < b) && (delta > b))) {
+        ret = a / b;
+    } else {
+        throw 0; //产生除0异常
+    }
+
+    return ret;
+}
+
 int main() {
-
-
-
-
-
-
-
-
-
-
+    try{
+        divide( 1.0 , 0);
+    }catch (int) {
+        cout << "divided 0" <<endl;
+    }
     return 0;
 }
